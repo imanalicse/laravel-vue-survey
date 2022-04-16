5fe516e896cc2164709bb2c5fc6cef03e3250dae
Iman <imanali.cse@gmail.com>
2022-04-16 19:45:08 +0600

    sign up

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)->mixedCase()->numbers()->symbols()
+            ],
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+}
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -9,27 +9,33 @@
        </router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="register">
     <input type="hidden" name="remember" value="true">
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
         <label for="full-name" class="sr-only">Full Name</label>
-        <input id="full-name" name="name" type="name" autocomplete="name" required
+        <input id="full-name" name="name" type="name" autocomplete="name" required v-model="user.name"
                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Full Name">
       </div>
 
       <div>
         <label for="email-address" class="sr-only">Email address</label>
-        <input id="email-address" name="email" type="email" autocomplete="email" required
+        <input id="email-address" name="email" type="email" autocomplete="email" required v-model="user.email"
                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Email address">
       </div>
       <div>
         <label for="password" class="sr-only">Password</label>
-        <input id="password" name="password" type="password" autocomplete="current-password" required
+        <input id="password" name="password" type="password" autocomplete="current-password" required v-model="user.password"
                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Password">
+      </div>
+      <div>
+        <label for="password_confirmation" class="sr-only">Password Confirmation</label>
+        <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation" required v-model="user.password_confirmation"
+               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+               placeholder="Password Confirmation">
       </div>
     </div>
 
@@ -51,12 +57,29 @@
   </form>
 </template>
 
-<script>
-export default {
-  name: "Register"
-}
+<script setup>
+  import { LockClosedIcon } from '@heroicons/vue/solid'
+  import store from "../store";
+  //import router from "../router";
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+  const user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  }
+
+  function register(ev) {
+    ev.preventDefault();
+    store
+    .dispatch('register', user)
+    .then((res) => {
+      router.push({
+        name: 'Dashboard'
+      })
+    })
+  }
+
 </script>
-
-<style scoped>
-
-</style>
